1dae732ad925af2c0dd0523c30c0c4db630ccc4e
“rene-weerts” <rjmwrt@gmail.com>
2023-08-04 15:13:14 +0200

    first commit

---
+++ src/main/java/comReneWeerts/Eindproject/Novi/AmbulantPedicureApplication.java
@@ -0,0 +1,13 @@
+package comReneWeerts.Eindproject.Novi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbulantPedicureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbulantPedicureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comReneWeerts/Eindproject/Novi/AmbulantPedicureApplicationTests.java
@@ -0,0 +1,13 @@
+package comReneWeerts.Eindproject.Novi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbulantPedicureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
